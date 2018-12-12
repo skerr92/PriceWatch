24841a1899fba0b59315fad7e34f29e65d175da9
Seth Kerr <skerr@aggies.ncat.edu>
2018-12-12 14:45:59 -0500

    Some actual working code to scrape the google news

--- check_price.py
+++ check_price.py
@@ -7,3 +7,31 @@
 #
 # Start Code after this line
 #
+
+from textblob import TextBlob
+import requests
+from bs4 import BeautifulSoup
+from sys import argv
+
+
+class Analysis:
+	def __init__(self, term):
+		self.term = term
+		self.price = 0
+
+
+		self.url = 'https://www.google.com/search?q={0}&source=lnms&tbm=nws'.format(self.term)
+
+	def run (self):
+		response = requests.get(self.url)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		headline_results = soup.find_all('div', class_='st')
+		for h in headline_results:
+			blob = TextBlob(h.get_text())
+			self.sentiment += blob.sentiment.polarity / len(headline_results)
+			self.subjectivity += blob.sentiment.subjectivity / len(headline_results)
+
+
+a = Analysis(argv[1])
+a.run()
+print(a.term, 'Subjectivity: ', a.subjectivity, 'Sentiment: ', a.sentiment)
