f9f4916962b2d9b03c3bae2623a5487687cf58a7
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2019-04-17 21:21:42 -0400

    Update Scraper.py
    Adding Twython Library with tweeting functionality.
    
    Also added OOP (object orientated programming) structure to the file as well.

--- Crypto_Price_App/Scraper.py
+++ Crypto_Price_App/Scraper.py
@@ -1,6 +1,7 @@
 import bs4
 import locale
 from urllib.request import urlopen as urlO
+from twython import Twython
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
@@ -8,42 +9,61 @@ import time
 
 locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
 
+# keys/secrets as strings in the following fields
+credentials = {}
+APP_KEY = ""
+APP_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+
+
+# Instantiate an object
+twitter = Twython(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+twitter.verify_credentials()
+twitter.get_home_timeline()
+
 # Reference URL
 my_url = 'https://coinmarketcap.com/'
 inittime = time.time()
 initdate_time = datetime.datetime.fromtimestamp(inittime).strftime('%Y-%m-%d %H:%M:%S')
+twitter.update_status(status="Seth Started This Tweet Stream for #BTC " + initdate_time + ".. updating every 5 minutes.")
 print('Initializing Time: ' + initdate_time)
 
-# Creating scheduling task
 
-def some_job():
-    # got info from page
-    starttime = time.time()
-    startdate_time = datetime.datetime.fromtimestamp(starttime).strftime('%Y-%m-%d %H:%M:%S')
-
-    print('Started pull at: ' + startdate_time)
-
+def get_crypto_price():
     uClient = urlO(my_url)
-    page_html= uClient.read()
+    page_html = uClient.read()
     uClient.close()
     # turn into beautiful soup
     page_soup = BeautifulSoup(page_html, "html.parser")
-    crypto = page_soup.findAll("td", {"class":"no-wrap currency-name"})
-    price = page_soup.findAll("td", {"class":"no-wrap text-right"})
+    crypto = page_soup.findAll("td", {"class": "no-wrap currency-name"})
+    price = page_soup.findAll("td", {"class": "no-wrap text-right"})
+    return price[0];
     # print("$"+containers[0].strong.text + containers[0].sup.text)
 
-    ts = time.time()
-    date_time = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+#Get current time stamp
+
+def get_timestamp():
+    starttime = time.time()
+    startdate_time = datetime.datetime.fromtimestamp(starttime).strftime('%Y-%m-%d %H:%M:%S')
+    return startdate_time;
+
+#Schedule The Task
+def some_job():
+    # got info from page
+    startdate_time = get_timestamp()
+
+    print('Started pull at: ' + startdate_time)
 
     #set lowest price as first index item
-    lowest_price = price[0].text
-    outputtext = 'Time Stamp of Last Price of ' + crypto[0].text + date_time + lowest_price
+    lowest_price = get_crypto_price().text
+    outputtext = 'Time Stamp of Last Price of #BTC ' + get_timestamp() + lowest_price
+    twitter.update_status(status=outputtext)
     print(outputtext)
 
 # run scheduler
 
 
 scheduler = BlockingScheduler()
-scheduler.add_job(some_job, 'interval', minutes=.25)
+scheduler.add_job(some_job, 'interval', minutes=5)
 scheduler.start()
-
