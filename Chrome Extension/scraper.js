f5e731c06c045165d1e42f1a6e876b1721335fdb
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2019-05-11 11:32:42 -0400

    Create scraper.js

---
+++ Chrome Extension/scraper.js
@@ -0,0 +1,9 @@
+/**
+*   This will be the heart of the scraper, where we will implement the code similar to the scraper.py script
+**/
+
+let url = "https://coinmarketcap.com/"
+
+let scraper = (url) => {
+  return url;
+}
